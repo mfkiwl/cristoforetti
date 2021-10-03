8b186df7ece55954337130f6b5be4bfcecad2ec4
Giulio Scattolin <giulio.scattolin@gmail.com>
2021-10-04 00:00:36 +0200

    feat: add TemporalOffset#toNanoseconds()

--- src/main/java/com/github/giulioscattolin/time/TemporalOffset.java
+++ src/main/java/com/github/giulioscattolin/time/TemporalOffset.java
@@ -1,4 +1,5 @@
 package com.github.giulioscattolin.time;
 
 public interface TemporalOffset {
+    double toNanoseconds();
 }
--- src/test/java/com/github/giulioscattolin/time/TemporalOffsetDummy.java
+++ src/test/java/com/github/giulioscattolin/time/TemporalOffsetDummy.java
@@ -1,4 +1,7 @@
 package com.github.giulioscattolin.time;
 
 class TemporalOffsetDummy implements TemporalOffset {
+    public double toNanoseconds() {
+        return 0;
+    }
 }
