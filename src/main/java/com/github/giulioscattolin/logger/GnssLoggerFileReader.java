16b58d15d7b4cbd9425b43cd25a4fb4482fa5d28
Giulio Scattolin <giulio.scattolin@gmail.com>
2021-10-04 21:05:14 +0200

    refactor: remove unused methods

--- src/main/java/com/github/giulioscattolin/logger/GnssLoggerFileReader.java
+++ src/main/java/com/github/giulioscattolin/logger/GnssLoggerFileReader.java
@@ -3,23 +3,9 @@ package com.github.giulioscattolin.logger;
 import com.github.giulioscattolin.data.*;
 
 public class GnssLoggerFileReader implements DataReader {
-    public Data read(DataSource source) {
-        if (source instanceof DataFile)
-            return read((DataFile) source);
-        else
-            return null;
-    }
-
     public void read(DataSource source, DataCollector collector) {
         if (source instanceof DataFile)
             read((DataFile) source, collector);
-    }
-
-    protected Data read(DataFile dataFile) {
-        if (isGnssLoggerPlainFile(dataFile))
-            return new FileAsData(dataFile);
-        else
-            return null;
     }
 
     private void read(DataFile dataFile, DataCollector collector) {
